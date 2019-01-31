474c2cacbfdf30a14c08634ec5fcc743998c11cf
lorenzo.santini <lorenzo.santini@wolterskluwer.com>
2019-01-31 09:44:14 +0100

    log

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var fs = require('fs');
 const main = require("./syncpom").main;
 var args = process.argv.splice(4);
+console.log(process.argv);
+console.log(args);
 var pomPath = args.length ? args[0] : 'pom.xml';
+console.log(args[0]);
 var packagePath = args.length >= 2 ? args[1] : 'package.json';
+console.log(args[1]);
 var devDependencyToUpdate = args.length >= 3 ? args[2] : undefined;
+console.log(args[2]);
 var devDependencyNewVersion = args.length >= 4 ? args[3] : undefined;
+console.log(args[3]);
 var UTF8 = 'UTF8';
 
 main(fs.readFileSync(pomPath, UTF8),
